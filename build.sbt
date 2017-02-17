c1fda8d6f9ebe92e6428744e37da4dc337cd9c21
Lars Hupel <lars.hupel@mytum.de>
2017-02-17 12:39:51 +0000

    solutions

--- build.sbt
+++ build.sbt
@@ -2,6 +2,8 @@ libraryDependencies += "org.typelevel" %% "cats" % "0.9.0"
 
 libraryDependencies += "org.scalacheck" %% "scalacheck" % "1.13.0"
 
+libraryDependencies += "org.typelevel" %% "algebra" % "0.6.0"
+
 addCompilerPlugin("org.spire-math" %% "kind-projector" % "0.9.3")
 
 scalaVersion := "2.12.1"
--- src/main/scala/Exercise4.scala
+++ src/main/scala/Exercise4.scala
@@ -1,5 +1,6 @@
 package katsconf
 
+import algebra.ring._
 import cats.kernel._
 import cats.implicits._
 
@@ -9,6 +10,18 @@ object Exercise4 {
 
     def randomIntMatrix(size: Int): Matrix[Int] =
       Matrix(List.fill(size)(List.fill(size)(scala.util.Random.nextInt(20) - 10)))
+
+    def unitMatrix[A](size: Int)(implicit r: Rig[A]): Matrix[A] =
+      Matrix(
+        (for (i <- 0 until size)
+        yield
+          (for (j <- 0 until size)
+          yield
+            if (i == j)
+              r.one
+            else
+              r.zero).toList).toList
+      )
 
   }
 
@@ -27,14 +40,57 @@ object Exercise4 {
 
     def +(that: Matrix[A])(implicit sg: Semigroup[A]): Matrix[A] = {
       require(this.size == that.size)
-      ???
+      Matrix(
+        for {
+          (row1, row2) <- (this.entries zip that.entries)
+        } yield (row1 zip row2) map { case (e1, e2) => e1 |+| e2 }
+      )
     }
 
     // Task 2: Implement scalar multiplication.
     // (Multiply each entry with a given scalar value)
 
-    def scale(r: A)(implicit sg: Semigroup[A]): Matrix[A] =
-      ???
+    def scale(r: A)(implicit sr: Semiring[A]): Matrix[A] =
+      Matrix(
+        for (row <- entries)
+        yield
+          for (item <- row) yield sr.times(r, item)
+      )
+
+    def *(that: Matrix[A])(implicit sr: Semiring[A]): Matrix[A] = {
+      require(this.size == that.size)
+      // There are multiple ways to implement this. I find this one pretty
+      // elegant, although it might not be very efficient.
+
+      // This turns a matrix
+      //   (a b c)
+      //   (d e f)
+      //   (g h i)
+      // into
+      //   (a d g)
+      //   (b e h)
+      //   (c f i)
+      // That way, we can iterate over the *columns* of the second matrix.
+      val transposed = that.entries.transpose
+
+      val res =
+        // for each row in the first matrix ...
+        for (row <- this.entries) yield
+          // ... and for each column in the second matrix ...
+          for (col <- transposed) yield {
+            // ... compute an entry in the result.
+
+            val zipped = row zip col
+            // here, zipped contains (a, b) pairs which need to be multiplied
+            val multiplied =
+              for ((x, y) <- zipped) yield
+                sr.times(x, y)
+            // now we just have to add those up
+            multiplied.foldLeft(sr.zero)(sr.plus)
+          }
+
+      Matrix(res)
+    }
   }
 
   // Task 3: Notice anything fishy?
